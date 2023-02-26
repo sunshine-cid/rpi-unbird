86ba831770706744ca4e2955ad53a3e7f390ac4a
Michael Donato Murphy <sunshine.cid@gmail.com>
2023-02-26 12:45:34 -0700

    Update install.sh
    Removed unnecessary commands
    Added option for custom user to be used
    Added download and extract of sound libraries
    Added echo of current status

--- install.sh
+++ install.sh
@@ -1,68 +1,42 @@
-# 02.19.23 
-# Currently this file lacks the links to download the sounds
-# Currnetly this install script should be used to copy/paste the necessary commands
-# Currently this file assumes a completely fresh rasbaian install.
+# 02.26.23 
+# Currently this file assumes a fresh and updated rasbaian install with appropriate networking configured.
 
-# Root Password Change
-passwd root
-# Add User
-adduser unbird
-#adduser unbird sudo,audio
-usermod -a -G audio,sudo unbird
+#Read potential command line flags and set variables
+#Current flags:
+#-u username - set username to build setup under, if empty use current user
 
-sudo /bin/sh -c 'echo "rpi-unbird-1" > /etc/hostname'
-sudo /bin/sh -c 'echo "127.0.0.1       localhost" > /etc/hosts'
-sudo /bin/sh -c 'echo "127.0.1.1       rpi-unbird-1" >> /etc/hosts'
+while getopts u: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        esac
+done
+# if username is null set it to current user
+if [ -z "$username" ]; then username="$USER"; fi
 
-dpkg-reconfigure tzdata
+echo "Username set as: $username";
 
-apt-get -y install raspi-config
-raspi-config
-
-apt-get update
-apt-get -y install sudo
-
-# Configure a way to persist commands after a reboot
-reboot
-
-sudo apt-get -y install wireless-tools
-sudo apt-get -y install wpasupplicant
-sudo apt-get -y install nano
-
-#Wifi Setup
-
-sudo /bin/sh -c 'echo "network={
-ssid="AircrackThis"
-key_mgmt=NONE
-wep_key0="0000000000"
-}" >> /etc/wpa_supplicant/wpa_supplicant.conf'
-
- 
-sudo /bin/sh -c 'echo "
-# The wifi (wireless) network interface
-auto wlan0
-allow-hotplug wlan0
-iface wlan0 inet dhcp
-       wireless-essid AircrackThis
-       wireless-key 0000000000" >> /etc/network/interfaces'
-
-#Fix WiFi 'Sleep" - Find a universal command?
-sudo /bin/sh -c 'echo "# Disable power management
-options 8192cu rtw_power_mgnt=0" >> /etc/modprobe.d/8192cu.conf'
-
-
+echo "Installing necessary software..."
 sudo apt-get -y install mpg321
 sudo apt-get -y install samba samba-common-bin
 
-
 # Download and extract sounds
-mkdir /home/unbird/sounds
-sudo chmod 0777 /home/unbird/sounds
+echo "Downloading sounds..."
+cd /home/$username/
+mkdir ./sounds
+cd sounds
+wget https://github.com/sunshine-cid/rpi-unbird/raw/master/hardcore.zip
+wget https://github.com/sunshine-cid/rpi-unbird/raw/master/silence.zip
+wget https://github.com/sunshine-cid/rpi-unbird/raw/master/z_listening.zip
+echo "Extracting sounds..."
+sudo unzip *.zip
+sudo chmod 0777 /home/$username/sounds
 
 #Scripts
-cd /home/unbird
+echo "Building scripts..."
+cd /home/$username
 mkdir scripts
-cd /home/unbird/scripts
+cd /home/$username/scripts
 sudo /bin/sh -c 'echo "mpg321 -Z /home/unbird/sounds/*.mp3" > weekday.sh'
 chmod uga+rwx weekday.sh
 sudo /bin/sh -c 'echo "mpg321 -Z /home/unbird/sounds/z_*.mp3" > weekend.sh'
@@ -71,21 +45,22 @@ sudo /bin/sh -c 'echo "pkill mpg321" > clockout.sh'
 chmod uga+rwx clockout.sh
 
 #Chron Jobs: Setup as root
+echo "Setting cron jobs..."
 sudo /bin/sh -c 'echo "
-0 9 * * 1,2,3,4,5 unbird /home/unbird/scripts/weekday.sh
+0 9 * * 1,2,3,4,5 unbird /home/$username/scripts/weekday.sh
 " > /etc/cron.d/weekday'
 sudo /bin/sh -c 'echo "
-0 9 * * 0,6 admin /home/unbird/scripts/weekend.sh
+0 9 * * 0,6 admin /home/$username/scripts/weekend.sh
 " > /etc/cron.d/weekend'
 sudo /bin/sh -c 'echo "
-0 17 * * * root home/unbird/scripts/clockout.sh
+0 17 * * * root home/$username/scripts/clockout.sh
 " > /etc/cron.d/clockout'
 
 # Samba setup - help from: http://raspberrywebserver.com/serveradmin/share-your-raspberry-pis-files-and-folders-across-a-network.html
-
+echo "Setting up Samba share..."
 sudo /bin/sh -c 'echo "[unbird/sounds]" >> /etc/samba/smb.conf'
 sudo /bin/sh -c 'echo "   comment= Where The Sounds Are Kept" >> /etc/samba/smb.conf'
-sudo /bin/sh -c 'echo "   path=/home/unbird/sounds" >> /etc/samba/smb.conf'
+sudo /bin/sh -c 'echo "   path=/home/$username/sounds" >> /etc/samba/smb.conf'
 sudo /bin/sh -c 'echo "   browseable=Yes" >> /etc/samba/smb.conf'
 sudo /bin/sh -c 'echo "   writeable=Yes" >> /etc/samba/smb.conf'
 sudo /bin/sh -c 'echo "   only guest=no" >> /etc/samba/smb.conf'
@@ -93,4 +68,6 @@ sudo /bin/sh -c 'echo "   create sudo /bin/sh -c 'mask=0777" >> /etc/samba/smb.c
 sudo /bin/sh -c 'echo "   directory mask=0777" >> /etc/samba/smb.conf'
 sudo /bin/sh -c 'echo "   public=no" >> /etc/samba/smb.conf'
 
-sudo smbpasswd -a unbird
+sudo smbpasswd -a $username
+
+echo "Done."
